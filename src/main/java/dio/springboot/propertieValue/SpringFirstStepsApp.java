083579b5f6e4a5a7cce4697228deccbbcfc837b1
Dominique Vieira <dominiquekv18@gmail.com>
2023-01-31 00:00:02 -0300

    msg

---
+++ src/main/java/dio/springboot/propertieValue/SpringFirstStepsApp.java
@@ -0,0 +1,12 @@
+package dio.springboot.propertieValue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFirstStepsApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFirstStepsApp.class, args);
+	}
+}
